5cbc20746d99b0f0f230a85c0e21021c3b0be1f1
HUYTV4 <huy8895@gmail.com>
2021-01-01 10:36:12 +0700

    predicate-trong-java-8

---
+++ src/com/training/predicate/PredicateExample1.java
@@ -0,0 +1,26 @@
+package com.training.predicate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateExample1 {
+    public static void main(String[] args) {
+
+        // Predicate String
+        Predicate<String> predicateString = s -> s.equals("gpcoder");
+        System.out.println(predicateString.test("gpcoder")); // true
+        System.out.println(predicateString.test("GP Coder")); // false
+
+        // Predicate integer
+        Predicate<Integer> predicateInt = i -> i > 0;
+        System.out.println(predicateInt.test(1)); // true
+        System.out.println(predicateInt.test(-1)); // false
+
+
+        List<String> strings = Arrays.asList("cat", "cow", "dog");
+        Predicate<String> containString = strings::contains;
+        System.out.println("containString.test(\"cat\") = " + containString.test("cat"));
+    }
+}
