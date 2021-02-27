afbf9bca4750f4da301b41e3694046feefac442f
huypc <huytv4>
2021-02-27 13:14:06 +0700

    optional example

---
+++ src/com/training/optional/OptionalExample2.java
@@ -0,0 +1,26 @@
+package com.training.optional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalExample2 {
+    public static void main(String[] args) {
+        // Pre java 8
+
+        // Java 8
+        String listOpt2 = getList2().orElse("orElse");
+        String listOpt3 = getList2().orElseGet(() -> String.valueOf(getList2().get()));
+
+        System.out.println("listOpt2 = " + listOpt2);
+        System.out.println("listOpt3 = " + listOpt3);
+    }
+
+    private static String getList() {
+        return "null";
+    }
+
+    private static Optional<String> getList2() {
+        return Optional.ofNullable("hehe");
+    }
+}
--- src/com/training/supplier/SupplierExample1.java
+++ src/com/training/supplier/SupplierExample1.java
@@ -4,13 +4,11 @@ import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class SupplierExample1 {
+
     public static void main(String[] args) {
 
         Supplier<String> supplier = () -> "Welcome to gpcoder.com";
         String hello = supplier.get();
         System.out.println(hello);
-
-        Stream.of("a", "b", "c").peek(e -> System.out.println("e = " + e)).forEach(System.out::println);
-        ;
     }
 }
